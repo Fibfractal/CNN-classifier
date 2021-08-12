6083a04167303fbdefd7a42e5f307103bb4442ad
Robert Nilsson <robert.nilsson.jobb@gmail.com>
2021-08-12 09:51:08 +0200

    Added store.js code

---
+++ frontend/src/store.js
@@ -0,0 +1,26 @@
+import { createStore } from 'vuex'
+
+const state = {
+  predictions: [],
+  images: []
+}
+
+const mutations = {
+  setPredictions(state, predictions) {
+    state.predictions = predictions
+  },
+  setImages(state, images) {
+    state.images = images
+  },
+  mergePredictImage(state){
+    for(let i = 0; i < state.predictions.length; i++){
+      state.predictions[i].img = state.images[i]
+      state.predictions[i].img_url = URL.createObjectURL(state.images[i])
+    }
+    state.predictions = state.predictions.sort((a,b) => (a.prediction > b.prediction) ? 1 : 
+    (a.prediction === b.prediction) ? ((a.probability > b.probability) ? 1 : -1) : -1);
+
+  },
+}
+
+export default createStore({ state, mutations })

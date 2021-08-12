cc98dbf3e9c8685cc2278b09d3a52ed1254976e2
Robert Nilsson <robert.nilsson.jobb@gmail.com>
2021-08-12 20:30:22 +0200

    Adjusted navbar for mobile

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -2,9 +2,15 @@
 
     <nav>
         <p>LOGO</p>
-        <router-link to="/">Import images</router-link>
-        <router-link to="/predictions">Classification</router-link>
-        <router-link to="/statistics">Diagram</router-link>
+
+        <div class="links">
+
+            <div class="container">
+                <router-link to="/">Import images</router-link>
+                <router-link to="/predictions">Classification</router-link>
+                <router-link to="/statistics">Diagram</router-link>
+            </div>
+        </div>
     </nav>
 
 </template>
@@ -30,24 +36,53 @@
 
     nav {
         display:flex;
-        justify-content: space-around;
+        align-items: center;
         background-color:rgb(206, 90, 90);
         margin-bottom: 40px;
-        height: 60px;
+        height: 70px;
         color: black;
         box-shadow: 0px 5px 10px  0px;
     }
 
+    .links {
+        margin: auto;
+    }
+
+    .container {
+        display: flex;
+        justify-content: center;
+    }
+
     a {
         text-decoration: none;
-        font-size: 20px;
         color: black;
-        margin: 10px;
     }
 
     a:hover {
         color: white;
         transition: 0.5s;
+    }
+
+    p {
+        float: left;
+    }
+
+    /* If the screen size is 601px wide or more, set the font-size of <div> to 22px */
+    @media screen and (min-width: 601px) {
+        a {
+            font-size: 22px;
+            margin-left: 30px;
+            margin-right: 30px;
+        }
+    }
+
+    /* If the screen size is 600px wide or less, set the font-size of <div> to 10px */
+    @media screen and (max-width: 600px) {
+        a {
+            font-size: 15px;
+            margin-left: 10px;
+            margin-right: 10px;
+        }
     }
 
 </style>

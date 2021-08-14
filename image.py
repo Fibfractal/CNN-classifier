f5d61f59d139dd416f77592cc0317977d3ca63a8
Robert Nilsson <robert.nilsson.jobb@gmail.com>
2021-08-14 14:50:13 +0200

    Added automatic image processing

--- image.py
+++ image.py
@@ -3,6 +3,7 @@ import aiofiles
 import cv2 
 from tensorflow.keras.preprocessing.image import load_img
 from tensorflow.keras.preprocessing.image import img_to_array
+import matplotlib.pyplot as plt
 
 
 async def save_image(file, app):
@@ -20,9 +21,39 @@ def load_image():
     # 1 channel
     image = load_img('uploads', grayscale=True, target_size=(28,28))
     image = img_to_array(image)
+    image = process_image(image)
+
     image = image.reshape(1,28,28,1)
     image = image.astype('float32') / 255
     return image
+
+def process_image(image):
+    # Get higher contrast
+    (thresh, image_t) = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
+
+    # See if white or black background
+    nbr_val = image_t.shape[0] * image_t.shape[1]
+    counter = 0
+    thresh = 0.5
+    
+    for k in range(len(image_t)):
+        for l in range(len(image_t[k])):
+            counter += 1 if image_t[k][l] == 255 else 0
+    
+    print("Counter:", counter)
+    percent_white = counter / nbr_val
+    print("Percent val white", percent_white)
+
+    if percent_white > thresh:
+        #invert color
+        image_t = (255-image_t)
+        
+        # plt.imshow(image_t)
+        # plt.show()
+        return image_t
+
+    return image_t
+
 
 
 def save_classified_image(prediction, file):

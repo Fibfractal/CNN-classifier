3bfd8a4a46e93e4c2cb335b1f926ef4f2a91ef54
Robert Nilsson <robert.nilsson.jobb@gmail.com>
2021-08-28 13:50:44 +0200

    No content push from side scrollbar, plus style

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -25,7 +25,24 @@
 
 </script>
 
-<style scoped>
+<style>
+
+  main {
+    width: 100vw;
+  }
+
+  ::-webkit-scrollbar {
+      height: 0px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: rgb(206, 90, 90,0.8);
+    border: 1px solid #141414;
+  }
+
+  body::-webkit-scrollbar-track {
+    background: rgb(185, 182, 182);
+  }
 
 </style>
 
